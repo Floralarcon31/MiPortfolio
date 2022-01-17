fc7fb66724bea11660e0fa1bd3de2745d25ff59a
floralarcon31 <floralarcon31@gmail.com>
2022-01-17 12:05:40 -0300

    agregue atributo target_blank y y boton descarga cv

--- js/main.js
+++ js/main.js
@@ -83,7 +83,7 @@ formulario.addEventListener('submit', handleSubmit)
 
 function handleSubmit(event) {
   event.preventDefault()
-  const form = new FormData(this)
+  const form= new FormData(this)
   buttonMailto.setAttribute('href', `mailto:floralarcon31@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
   buttonMailto.click()
 }
